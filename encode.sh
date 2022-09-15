f193b06c50a2c21e90c1f571dee2d34f32bddd7a
mingchen01 <920879227@qq.com>
2022-09-15 23:09:59 +0800

    Create encode.sh

---
+++ encode.sh
@@ -0,0 +1,79 @@
+baseCode=(`echo {A..Z} {a..z} {0..9} + /`)
+declare -A baseIndexCode
+for i in ${!baseCode[@]};do
+    baseIndexCode[${baseCode[$i]}]=$i
+done
+
+
+#错误码
+ERR_NULLPARM=2
+
+
+#base64编码
+baseEncode(){
+for((i=0;i<${#binaryStr};i+=24));do
+    buffers=${binaryStr:$i:24}
+
+    [ ${#buffers} -lt 24 ] && eqFlag=true
+
+    buffers+=`echo 000000000000000000000000|head -c $[24-${#buffers}]`
+    for((j=0;j<${#buffers};j+=6));do
+        tempBin=${buffers:$j:6}
+        #echo $tempBin
+        if [ $eqFlag = true ] && [ `echo "ibase=2;$tempBin"|bc` -eq 0 ];then
+            dataStr+="="
+        else
+            dataStr+=${baseCode[`echo "ibase=2;$tempBin"|bc`]}
+
+        fi
+    done
+done
+}
+#base64解码
+baseDecode(){
+for((i=0;i<${#binaryStr};i+=8));do
+    buffers=${binaryStr:$i:8}
+
+    [ ${#buffers} -lt 8 ] && break;
+    dataStr+=`echo "ibase=2;$buffers"|bc|awk '{printf("%c"),$buffers}'`
+    #awk中的$buffers是awk读取的标准输入，在单引号内，不会被变量替换。
+
+done
+}
+
+
+#临时变量
+binaryStr=""
+dataStr=""
+eqFlag=false
+
+[ $# -lt 2 ] && { echo "Usage: `basename $0` str encode|decode ";exit $ERR_NULLPARM; }
+
+#展开二进制位
+for((i=0;i<${#1};i++));do
+    if [ "$2" = "encode" ];then
+        binaryStr+=$(echo "obase=2;`printf "%d" "'${1:$i:1}"`"|bc|xargs printf "%08d")
+    elif [ "$2" = "decode" ];then
+        [ ${1:$i:1} = "=" ] && continue
+        binaryStr+=$(echo "obase=2;${baseIndexCode[${1:$i:1}]}"|bc|xargs printf "%06d")
+        #echo ${baseIndexCode[${1:$i:1}]}
+        #echo $binaryStr
+    fi
+done
+
+#echo $binaryStr
+#echo ${#binaryStr}
+
+
+case "$2" in
+"encode")
+    baseEncode
+    ;;
+"decode")
+    baseDecode
+    ;;
+*)
+    ;;
+esac
+
+echo $dataStr
